d22897e46019ebf3c6e6fd433d1bf8f728143751
Donner Castro <donnercastro@Macbook-Air-Bought-in-2024.local>
2025-04-06 18:39:02 -0500

    Updated index.js with basic functionality

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-console.log('USD-PEN Forecast Tool Loaded');
+// Log a message to the console
+console.log("USD to PEN Forecast Tool is working!");
+
+// You could add more functionality here, such as fetching data, handling user input, etc.
